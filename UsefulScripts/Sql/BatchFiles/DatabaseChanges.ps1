54009827783e3bccc45a9b5d9591c6124e0fdb01
SimonElms <simone@datacom.co.nz>
2023-12-02 23:58:15 +1300

    databasechanges_2: Automatically install required PowerShell modules

--- UsefulScripts/Sql/BatchFiles/DatabaseChanges.ps1
+++ UsefulScripts/Sql/BatchFiles/DatabaseChanges.ps1
@@ -14,9 +14,9 @@ file, which can be kept as a record of the results of a deployment.
 Author:			Simon Elms
 Requires:		PowerShell 5.1 or greater
                 sqlcmd, the SQL Server Commandline Utility, which needs to be on the Windows PATH.
-                SqlServer PowerShell module (install from PowerShell Gallery, not installed as 
-                                            part of SQL Server)
-Version:		1.3.0
+                PowerShell modules:
+                    (these will be installed automatically if not already installed)
+                    SqlServer
 Date:			2 Dec 2023
 
 When listing the SQL script file names, the file extensions are optional.  So a SQL script file 
@@ -141,9 +141,111 @@ $_logFileName = "release.log"
 # If set overwrites any existing log file.  If cleared appends to an existing log file.  If 
 # no log file exists a new one will be created, regardless of the setting of this variable.
 $_overwriteLogFile = $True 
-$_verboseLoggingOn = $True
+$_sqlVerboseLoggingOn = $True
+
+$_requiredModules = @('SqlServer')
+$_moduleRepository = 'PSGallery'
+$_proxyServerUrl = ''
 
 #endregion Configuration **************************************************************************
+
+<#
+.SYNOPSIS
+Checks whether the specified module is already installed and installs it if it isn't.
+
+.DESCRIPTION
+If the specified module is not already installed the function will attempt to install it 
+assuming it has direct access to the repository.  If that fails it will attempt to install the 
+module via a proxy server.
+
+If this function installs a module it will be installed for the current user only, not for all 
+users of the computer.
+
+.NOTES
+Cannot include logging in this function because it will be used to install the logging module 
+if it's not already installed.
+#>
+function Install-RequiredModule (
+    [string]$ModuleName,
+    [string]$RepositoryName,
+    [string]$ProxyUrl
+    )
+{
+    Write-Output "Checking whether PowerShell module '$ModuleName' is installed..."
+
+    # "Get-InstalledModule -Name <module name>" will throw a non-terminating error if the module 
+    # is not installed.  Don't want to display the error so silently continue.
+    if (Get-InstalledModule -Name $ModuleName `
+        -ErrorAction SilentlyContinue -WarningAction SilentlyContinue)
+    {
+        Write-Output "Module '$ModuleName' is installed."
+        return
+    }
+    
+    Write-Output "Installing PowerShell module '$ModuleName'..."
+
+    # Repository probably has too many modules to enumerate them all to find the name.  So call 
+    # "Find-Module -Repository $RepositoryName -Name $ModuleName" which will raise a 
+    # non-terminating error if the module isn't found.
+
+    # Silently continue on error because the error message isn't user friendly.  We'll display 
+    # our own error message if needed.
+    if ((Find-Module -Repository $RepositoryName -Name $ModuleName `
+        -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).Count -eq 0)
+    {
+        throw "Module '$ModuleName' not found in repository '$RepositoryName'.  Exiting."
+    }
+
+    try
+    {
+        # Ensure the repository is trusted otherwise the user will get an "untrusted repository"
+        # warning message.
+        $repositoryInstallationPolicy = (Get-PSRepository -Name $RepositoryName |
+                                            Select-Object -ExpandProperty InstallationPolicy)
+        if ($repositoryInstallationPolicy -ne 'Trusted')
+        {
+            Set-PSRepository -Name $RepositoryName -InstallationPolicy Trusted
+        }
+    }
+    catch 
+    {
+        throw "Module repository '$RepositoryName' not found.  Exiting."
+    }
+    
+    try
+    {        
+        # If Install-Module fails because it's behind a proxy we want to fail silently, without 
+        # displaying any message to scare the user.  
+        # Errors from Install-Module are non-terminating.  They won't be caught using try - catch 
+        # unless ErrorAction is set to Stop. 
+        Install-Module -Name $ModuleName -Repository $RepositoryName `
+            -Scope CurrentUser -ErrorAction Stop -WarningAction SilentlyContinue
+    }
+    catch 
+    {
+        # Try again, this time with proxy details, if we have them.
+
+        if ([string]::IsNullOrWhiteSpace($ProxyUrl))
+        {
+            throw "Unable to install module '$ModuleName' directly and no proxy server details supplied.  Exiting."
+        }
+
+        $proxyCredential = Get-Credential -Message 'Please enter credentials for proxy server'
+
+        # No need to Silently Continue this time.  We want to see the error details.  Convert 
+        # non-terminating errors to terminating via ErrorAction Stop.   
+        Install-Module -Name $ModuleName -Repository $RepositoryName `
+            -Proxy $ProxyUrl -ProxyCredential $proxyCredential `
+            -Scope CurrentUser -ErrorAction Stop
+    }
+
+    if (-not (Get-InstalledModule -Name $ModuleName -ErrorAction SilentlyContinue))
+    {
+        throw "Unknown error installing module '$ModuleName' from repository '$RepositoryName'.  Exiting."
+    }
+
+    Write-Output "Module '$ModuleName' successfully installed."
+}
 
 <#
 .SYNOPSIS
@@ -776,8 +878,6 @@ function Update-Database (
     [switch]$OverwriteLogFile
     )
 {
-    Clear-Host 
-
     $LogFileName = Get-AbsolutePath $LogFileName
 
     $fileNameIsValid = Test-Path $LogFileName -IsValid
@@ -846,7 +946,14 @@ function Update-Database (
     Write-LogMessage " " -LogFileName $LogFileName -WriteRawMessageOnly
 }
 
+Clear-Host 
+
+foreach($module in $_requiredModules)
+{
+    Install-RequiredModule -ModuleName $module -RepositoryName $_moduleRepository -ProxyUrl $_proxyServerUrl
+}
+
 
 Update-Database -SqlServers $_sqlServers -DefaultDatabase $_defaultDatabase `
-    -SqlScriptNames $_sqlScriptNames -VerboseLoggingOn:$_verboseLoggingOn `
-    -LogFileName $_logFileName -OverwriteLogFile:$_overwriteLogFile
+    -SqlScriptNames $_sqlScriptNames -SqlVerboseLoggingOn:$_sqlVerboseLoggingOn `
+    -LogFileName $_logFileName
