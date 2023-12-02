f46fbbe102939bce067f054942dd3c69d6474f39
SimonElms <simone@datacom.co.nz>
2023-12-02 18:31:41 +1300

    Move array demos to separate folder

---
+++ DemosAndExperiments/Arrays/DEMO_Arrays_Intersection.ps1
@@ -0,0 +1,21 @@
+﻿<#
+.SYNOPSIS
+Demonstrates how to get the intersection of two arrays.
+
+.NOTES
+Based on Stackoverflow question "Union and Intersection in PowerShell?", 
+https://stackoverflow.com/questions/8609204/union-and-intersection-in-powershell
+
+#>
+
+Clear-Host
+
+$firstArray = @(1,2,3,4,5)
+$secondArray = @(4,5,6,7,8)
+
+# Will not work if first array is null.
+$intersection1 = Compare-Object $firstArray $secondArray -PassThru -IncludeEqual -ExcludeDifferent
+$intersection1
+
+$intersection2 = $firstArray | Where-Object { $secondArray -contains $_ }
+$intersection2
