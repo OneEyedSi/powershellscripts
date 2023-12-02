1bff9dec02a0bdc14942d3111d4d0a42013d1446
SimonElms <simone@datacom.co.nz>
2023-12-02 18:37:32 +1300

    Add null comparison demos

---
+++ DemosAndExperiments/DEMO_Null_Comparison.ps1
@@ -0,0 +1,116 @@
+﻿<#
+.SYNOPSIS
+Demonstrates why $Null should appear on the left side of a comparison.
+
+.DESCRIPTION
+Demonstrates why $Null should appear on the left side of a comparison.
+
+.NOTES
+Author:			Simon Elms
+Requires:		PowerShell 5.1 or later 
+Version:		1.0.0 
+Date:			22 Oct 2023
+
+Putting the $Null on the left of the comparison ensures a boolean result is returned from the 
+comparison.  
+
+If the $InputValue is a collection and the $Null is on the right, the comparison would return an 
+array of matching values (ie an array of every occurrence of $Null in the collection under test), 
+or an empty array if there were no $Nulls in the collection.
+
+So get into the habit of putting $Null on the left in comparisons.
+
+See "PossibleIncorrectComparisonWithNull", 
+https://learn.microsoft.com/en-us/powershell/utility-modules/psscriptanalyzer/rules/possibleincorrectcomparisonwithnull?view=ps-modules
+for a full explanation.
+
+#>
+
+function Get-ValueDataType ($InputValue)
+{
+    if ($Null -eq $InputValue)
+    {
+        return '[NULL]'
+    }
+
+    $dataType = $InputValue.GetType().Name
+
+    if ($InputValue -is [array])
+    {
+        $numberOfElements = $InputValue.Count
+        $dataType += " (array of $numberOfElements elements)"
+    }
+
+    return $dataType
+}
+
+function Write-Result ($InputValue)
+{
+    $result = ($Null -eq $InputValue)
+    Write-Host "Null on left: $result"
+    $resultType = Get-ValueDataType $result
+    Write-Host "Result type: $resultType"
+
+    $result = ($InputValue -eq $Null)
+    Write-Host "Null on right: $result"
+    $resultType = Get-ValueDataType $result
+    Write-Host "Result type: $resultType"
+
+    Write-Host '---------------------'
+}
+
+Clear-Host
+
+# Result:
+<#
+Null on left: True
+Result type: Boolean
+Null on right: True
+Result type: Boolean
+#>
+Write-Result $Null
+
+# Result:
+<#
+Null on left: False
+Result type: Boolean
+Null on right: 
+Result type: Object[] (array of 0 elements)
+#>
+Write-Result @()
+
+# Result:
+<#
+Null on left: False
+Result type: Boolean
+Null on right: 
+Result type: Object[] (array of 1 elements)
+#>
+Write-Result @($Null)
+
+# Result:
+<#
+Null on left: False
+Result type: Boolean
+Null on right:
+Result type: Object[] (array of 1 elements)
+#>
+Write-Result @(1, 2, $Null)
+
+# Result:
+<#
+Null on left: False
+Result type: Boolean
+Null on right:
+Result type: Object[] (array of 0 elements)
+#>
+Write-Result @(1, 2)
+
+# Result:
+<#
+Null on left: False
+Result type: Boolean
+Null on right:
+Result type: Object[] (array of 2 elements)
+#>
+Write-Result @(1, 2, $Null, 3, 4, $Null)
---
+++ DemosAndExperiments/DEMO_Variable_Distinguish_Null_False.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.SYNOPSIS
+Demonstrates a test that distinguishes between $Null and $False.
+
+.DESCRIPTION
+Demonstrates a test that distinguishes between $Null and $False.  This allows a nullable boolean 
+variable to be used ($Null = not set; $True/$False = set to either true or false).
+
+.NOTES
+Author:			Simon Elms
+Requires:		PowerShell 5.1 or later 
+Version:		1.0.0 
+Date:			22 Oct 2023
+
+#>
+
+function Write-WhatSet ($InputValue)
+{
+    # $Null on left of comparison ensures a boolean result is returned from the comparison.
+    # If the $InputValue were a collection and the $Null were on the right, the comparison 
+    # would return a matching value, or an empty array if there were no matches.
+    # While this particular demo doesn't involve collection input values it's a good idea to get 
+    # into the habit of putting $Null on the left.
+    if ($Null -eq $InputValue)
+    {
+        Write-Host 'Is NULL'
+        return
+    }
+
+    Write-Host "Is $InputValue"
+}
+
+Write-WhatSet $null
+Write-WhatSet $true
+Write-WhatSet $false
