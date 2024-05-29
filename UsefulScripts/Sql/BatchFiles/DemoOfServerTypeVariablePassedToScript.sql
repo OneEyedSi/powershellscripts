52b2c62428b76637d30274614c2aff84856dc798
SimonElms <simone@datacom.co.nz>
2024-05-29 19:38:19 +1200

    Add SQL batch file DemoOfServerTypeVariablePassedToScript

---
+++ UsefulScripts/Sql/BatchFiles/DemoOfServerTypeVariablePassedToScript.sql
@@ -0,0 +1,28 @@
+﻿-- Demonstrates how the ServerType SQLCMD variable can alter script behaviour,
+-- depending on the server selected by the user.
+
+DECLARE @ValueToInsert NVARCHAR(255) = 
+    CASE '$(ServerType)'
+        WHEN 'LOCALDB' THEN 'LOCAL DEV SERVER'
+        WHEN 'DEV' THEN 'REMOTE DEV SERVER'
+        WHEN 'TEST' THEN 'TEST SERVER'
+        WHEN 'UAT' THEN 'UAT SERVER'
+        WHEN 'LIVE' THEN 'PRODUCTION SERVER'
+        ELSE 'UNKNOWN SERVER'
+    END;
+
+MERGE INTO TestTable1 AS Target
+USING (VALUES
+        (1, @ValueToInsert)
+    ) AS Source ([ID], [Name])
+ON Target.[ID] = Source.[ID] 
+
+--Update Source when ID Matched.
+WHEN MATCHED THEN 
+UPDATE SET [Name] = Source.[Name], 
+    UpdateDate = SYSDATETIMEOFFSET() AT TIME ZONE 'New Zealand Standard Time'
+
+--Add New When Not in Target
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT ([ID], [Name]) 
+VALUES (Source.[ID], Source.[Name]);
