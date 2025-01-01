e6c5a73f74afaaa4e99e91e74f44e7f257c6f69c
Simon Elms <simon.elms@datacom.com>
2025-01-02 02:47:08 +1300

    Add demo of mocking a function that updates a hashtable

---
+++ DemosAndExperiments/PesterTesting/Mocking/Mocking_FunctionThatUpdatesHashTable.Tests.ps1
@@ -0,0 +1,60 @@
+﻿<#
+.SYNOPSIS
+Demonstrates mocking a function that updates a hashtable.
+
+.NOTES
+Author:			Simon Elms
+Requires:		Pester v5.5 or v.5.6 PowerShell module
+Version:		1.0.0 
+Date:			1 Jan 2025
+
+The following scripts belong together:
+* Mocking_FunctionThatUpdatesHashTable.ps1:         Function under test
+* Mocking_FunctionThatUpdatesHashTable.Tests.ps1:   Tests
+
+Normally the function under test could be included in the .Tests file, in a BeforeAll block.  However, if you wish to run 
+the function under test manually in the top-level code of a script you'll have to move it to a different file.  This is 
+because PowerShell recognises .Tests.ps1 files as Pester files and runs the tests, rather than running any normal 
+top-level code in the file.  
+
+Even moving the function under test to a different file would usually result in the top-level code in that file running 
+during the Pester discovery phase.  To avoid that we're using the $InTestContext script parameter to disable the running 
+of the top-level code in the file under test during discovery and run phases of Pester tests.  Running the file under 
+test normally, as opposed to via Pester, will not set $InTestContext and will allow the top-level code in the file to run.
+
+#>
+
+#region Configuration ******************************************************************************************************
+
+# Can't dot source using ". .\Mocking_FunctionThatUpdatesHashTable.ps1" (without quotes) as relative paths are relative 
+# to the current working directory, not the directory this test file is in.  So Use $PSScriptRoot to get the directory this 
+# file is in, and dot source the file to test from the same directory.
+# -InTestContext switch ensures the script under test doesn't run automatically when dot sourced into this script.
+BeforeAll {
+    # Must dot-source file to test in BeforeAll.
+    . (Join-Path $PSScriptRoot 'Mocking_FunctionThatUpdatesHashTable.ps1') -InTestContext
+}
+
+#endregion Configuration ***************************************************************************************************
+
+#region Pester tests *******************************************************************************************************
+
+Describe 'Set-Something' {
+    BeforeAll {
+        Mock Update-HashTable {  
+            $HashTable.State = 'Test State'
+        } 
+    }
+    BeforeEach {
+        $ht = @{Name='Name'; Description='Description'}
+    }
+
+    It 'sets the State for the supplied hashtable' {
+        Set-Something $ht
+
+        $ht.ContainsKey('State') | Should -BeTrue
+        $ht.State | Should -BeExactly 'Test State'
+    }
+}
+
+#endregion Pester tests ****************************************************************************************************
---
+++ DemosAndExperiments/PesterTesting/Mocking/Mocking_FunctionThatUpdatesHashTable.ps1
@@ -0,0 +1,112 @@
+﻿<#
+.SYNOPSIS
+Demonstrates mocking a function that updates a hashtable.
+
+.NOTES
+Author:			Simon Elms
+Requires:		Pester v5.5 or v.5.6 PowerShell module
+Version:		1.0.0 
+Date:			1 Jan 2025
+
+The following scripts belong together:
+* Mocking_FunctionThatUpdatesHashTable.ps1:         Function under test
+* Mocking_FunctionThatUpdatesHashTable.Tests.ps1:   Tests
+
+Normally the function under test could be included in the .Tests file, in a BeforeAll block.  However, if you wish to run 
+the function under test manually in the top-level code of a script you'll have to move it to a different file.  This is 
+because PowerShell recognises .Tests.ps1 files as Pester files and runs the tests, rather than running any normal 
+top-level code in the file.  
+
+Even moving the function under test to a different file would usually result in the top-level code in that file running 
+during the Pester discovery phase.  To avoid that we're using the $InTestContext script parameter to disable the running 
+of the top-level code in the file under test during discovery and run phases of Pester tests.  Running the file under 
+test normally, as opposed to via Pester, will not set $InTestContext and will allow the top-level code in the file to run.
+
+#>
+
+param ([switch]$InTestContext)
+
+#region Functions Under Test ***********************************************************************************************
+
+function Update-HashTable (
+    [hashtable]$HashTable
+)
+{
+    $HashTable.State = "State added"
+}
+
+function Set-Something ([hashtable]$HashTable)
+{
+    Update-HashTable $HashTable
+}
+
+#endregion Functions Under Test ********************************************************************************************
+
+#region Manual check script ************************************************************************************************
+
+# Manually run script, which doesn't run during tests, that demonstrates the function under test works as intended.
+
+function Write-Hashtable (
+    [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+    [hashtable]$HashTable, 
+
+    [string]$Title
+)
+{
+    process
+    {
+        Write-Host $Title
+
+        # Hashtable.Keys is not sortable so create an array of the keys and sort that.
+        $keys = $HashTable.Keys | Sort-Object
+        foreach($key in $keys)
+        {
+            $Value = $HashTable[$key] 
+
+            try
+            {
+                $Type = " (type $($Value.GetType().FullName))"
+            }
+            catch
+            {
+                $Type = ""
+            }
+                
+            if ($Null -eq $Value)
+            {
+                $Value = "[NULL]"
+            }
+            elseif ($Value -is [string] -and $Value -eq "")
+            {
+                $Value = "[EMPTY STRING]"
+            }
+            elseif ($Value -is [string] -and $Value.Trim() -eq "")
+            {
+                $Value = "[BLANK STRING]"
+            }
+
+            Write-Host "    [$key] $Type : '$Value'"
+        }
+
+        Write-Host
+    }
+}
+
+# Ensure the script below doesn't run as part of the Pester discovery or run phases.
+if($InTestContext)
+{
+    return
+}
+
+$hashtable = @{Name='Name 1'; Description='Description 1'}
+
+Clear-Host 
+
+Write-Hashtable $hashtable 'Initial hashtable'
+
+Set-Something $hashtable
+
+Write-Hashtable $hashtable 'hashtable after update'
+
+#endregion Manual check script script **************************************************************************************
+
