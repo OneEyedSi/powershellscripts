2332a0b4b7c7a729de14d2d2a13319945fc2d177
Simon Elms <simon.elms@datacom.com>
2025-08-16 00:30:41 +1200

    Add dot sourcing via multiple reference chains demo

---
+++ DemosAndExperiments/DotSourcing/MultipleChains/LowestLevel.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+Lowest level script in a demo of dot sourcing with multiple reference chains between the lowest level script and 
+the highest level script.
+
+.DESCRIPTION
+
+.NOTES
+Author:			Simon Elms
+Requires:		Windows PowerShell 5.1 or cross-platform PowerShell 6+
+Version:		1.0.0 
+Date:			15 Aug 2025
+
+The reference chains from one script to another:
+
+                TopLevel.ps1
+                    |
+      +-------------+-------------+
+      |                           |
+SecondLevel1.ps1            SecondLevel2.ps1
+      |                           |
+      +-------------+-------------+
+                    |
+              LowestLevel.ps1   
+
+#>
+
+function Write-Text ([string]$Text)
+{
+    Write-Host $Text
+}
---
+++ DemosAndExperiments/DotSourcing/MultipleChains/SecondLevel2.ps1
@@ -0,0 +1,34 @@
+<#
+.SYNOPSIS
+Second level script in a demo of dot sourcing with multiple reference chains between the lowest level script and 
+the highest level script.
+
+.DESCRIPTION
+
+.NOTES
+Author:			Simon Elms
+Requires:		Windows PowerShell 5.1 or cross-platform PowerShell 6+
+Version:		1.0.0 
+Date:			15 Aug 2025
+
+The reference chains from one script to another:
+
+                TopLevel.ps1
+                    |
+      +-------------+-------------+
+      |                           |
+SecondLevel1.ps1            SecondLevel2.ps1
+      |                           |
+      +-------------+-------------+
+                    |
+              LowestLevel.ps1   
+
+#>
+
+. $PSScriptRoot\LowestLevel.ps1
+
+function Write-SecondLevel2 ([string]$Text)
+{
+    $message = "SecondLevel2: $Text"
+    Write-Text $message
+}
